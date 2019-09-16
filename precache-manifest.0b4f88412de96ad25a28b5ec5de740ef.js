ed3435aa2eb087cd710a311a982d70db6247bf01
Ramy Wassef <ramywroshdy@gmail.com>
2019-09-16 11:10:57 +0200

    Updates

---
+++ precache-manifest.0b4f88412de96ad25a28b5ec5de740ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7ebdda18e707ecd5dcac27423c2822",
+    "url": "/Crud-system/index.html"
+  },
+  {
+    "revision": "851163fcea74e8278cc1",
+    "url": "/Crud-system/static/css/main.f1db5722.chunk.css"
+  },
+  {
+    "revision": "c6b43bfa386d738a0093",
+    "url": "/Crud-system/static/js/2.2c1938a2.chunk.js"
+  },
+  {
+    "revision": "851163fcea74e8278cc1",
+    "url": "/Crud-system/static/js/main.344eb6b5.chunk.js"
+  },
+  {
+    "revision": "5c8e56c083a8349e4dd6",
+    "url": "/Crud-system/static/js/runtime~main.e7bfd111.js"
+  }
+]);
